e9efc847903d03126c0b929dbb85803c78965a47
Julian Lopez <a20490712@itmexicali.edu.mx>
2022-12-21 17:21:19 -0800

    Finished home page

--- static/js/test.js
+++ static/js/test.js
@@ -5,3 +5,7 @@ function closeMenu() {
 function openMenu() {
     document.getElementById("sidebar").style.width = "225px";
 }
+
+function dropdown() {
+    document.getElementById("usr-menu").style.visibility = "visible";
+}
